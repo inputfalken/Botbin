c1bedea4d496dad0ad32390becedb8d42bb9c201
Robert Andersson <inputfalken@gmail.com>
2017-10-13 21:36:04 +0200

    Add save command

--- Botbin/CommandCenters/UserEventsCommandCenter.cs
+++ Botbin/CommandCenters/UserEventsCommandCenter.cs
@@ -1,19 +1,25 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Botbin.GameTracking;
 using Botbin.GameTracking.UserEvent.Enums;
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace Botbin.CommandCenters {
     public class UserEventsCommandCenter : ModuleBase<SocketCommandContext> {
+        private readonly IUserEventRetriever _eventRetriever;
+
+        public UserEventsCommandCenter() => _eventRetriever = Program.Services.GetService<IUserEventRetriever>();
+
         [Command("history", RunMode = RunMode.Async)]
         [Summary("Retrieves the  game history of the user.")]
         public async Task History([Summary("The (optional) user to get info for")] SocketUser user = null) {
             var userInfo = user ?? Context.Client.CurrentUser;
-            var userEvents = await Program.Services
-                .GetService<IUserEventRetriever>()
+            var userEvents = await _eventRetriever
                 .UserEventsById(userInfo.Id)
                 .ToAsyncEnumerable()
                 .ToArray();
@@ -29,6 +35,25 @@ namespace Botbin.CommandCenters {
             else
                 await Context.Channel.SendMessageAsync(
                     $"No game history found for user '{userInfo.Username}#{userInfo.Discriminator}'");
+        }
+
+        [Command("save", RunMode = RunMode.Async)]
+        public async Task Save() {
+            const ulong admin = 318468838058360846;
+            if (Context.User.Id == admin) {
+                var userEvents = await _eventRetriever
+                    .UserEvents()
+                    .ToAsyncEnumerable()
+                    .ToArray();
+                if (userEvents.Length > 0) {
+                    await File.WriteAllTextAsync(".\\history.json", JsonConvert.SerializeObject(userEvents));
+                    await Context.Channel.SendMessageAsync("Successfully saved content to disc.");
+                }
+                else {
+                    await Context.Channel.SendMessageAsync("No data available to save.");
+                }
+            }
+            else await Context.Channel.SendMessageAsync("Pfft, i wont save this. You are not the boss of me!");
         }
     }
 }
