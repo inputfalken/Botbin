98fcf689d893e4be85071e1ceda0733d92262395
Robert Andersson <inputfalken@gmail.com>
2017-10-22 19:53:22 +0200

    Add gamehistory command for everyone and dry existing code

--- Botbin/CommandCenters/UserEventModule.cs
+++ Botbin/CommandCenters/UserEventModule.cs
@@ -22,28 +22,55 @@ namespace Botbin.CommandCenters {
             _settings = provider.GetService<Settings>();
         }
 
+        private static string FormatActivities(IEnumerable<IUserEvent> events, string header = "Activities")
+            => events
+                .OrderBy(e => e.Time)
+                .Aggregate(
+                    $"__**{header}**__:{NewLine}```",
+                    (a, c) => $"{a}{c.Username} {c.Action} at {c.Time}{NewLine}"
+                    , s => $"{s}```"
+                );
+
+        private static string FormatGameHistory(IEnumerable<UserGame> events, string header = "Game History")
+            => events
+                .OrderBy(e => e.Time)
+                .Aggregate(
+                    $"__**{header}**__:{NewLine}```",
+                    (a, c) => $"{a}{c.Username} {c.Action} {c.Game.Name} at {c.Time}{NewLine}"
+                    , s => $"{s}```"
+                );
+
         [Command("gamehistory", RunMode = RunMode.Async)]
         [Summary("Retrieves the  game history of the user.")]
-        public async Task GameHistory([Summary("The (optional) user to get info for")] SocketUser user = null) {
-            var userInfo = user ?? Context.Client.CurrentUser;
+        public async Task GameHistory([Summary("The (optional) user to get info for")] SocketUser user) {
             var userGames = _eventRetriever
-                .UserEventsById(userInfo.Id)
+                .UserEventsById(user.Id)
                 .Where(u => u is UserGame)
                 .Cast<UserGame>()
                 .OrderBy(game => game.Time);
 
-            if (userGames.Any()) {
-                var msg = userGames.Aggregate(
-                    $"__**{userInfo.Username} Game History**__:{NewLine}```",
-                    (a, c) => $"{a}{c.Action} {c.Game.Name} at {c.Time}{NewLine}",
-                    s => $"{s}```"
+            if (userGames.Any())
+                await Context.Channel.SendMessageAsync(FormatGameHistory(
+                    userGames,
+                    $"Game History for '{user.Username}'")
                 );
-                await Context.Channel.SendMessageAsync(msg);
-            }
-            else {
+            else
                 await Context.Channel.SendMessageAsync(
-                    $"No game history found for user '{userInfo.Username}#{userInfo.Discriminator}'");
-            }
+                    $"No game history found for user '{user.Username}'"
+                );
+        }
+
+        [Command("gamehistory", RunMode = RunMode.Async)]
+        [Summary("Retrieves the  game history for everyone.")]
+        public async Task GameHistory() {
+            var userGames = _eventRetriever
+                .UserEvents()
+                .Where(u => u is UserGame)
+                .Cast<UserGame>()
+                .OrderBy(game => game.Time);
+
+            if (userGames.Any()) await Context.Channel.SendMessageAsync(FormatGameHistory(userGames));
+            else await Context.Channel.SendMessageAsync("No game history found.");
         }
 
         [Command("save", RunMode = RunMode.Async)]
@@ -70,19 +97,11 @@ namespace Botbin.CommandCenters {
             else await Context.Channel.SendMessageAsync("Could not find any activity.");
         }
 
-        private static string FormatActivities(IEnumerable<IUserEvent> events)
-            => events
-                .OrderBy(e => e.Time)
-                .Aggregate(
-                    $"__**Activities**__:{NewLine}```",
-                    (a, c) => $"{a}{c.Username} {c.Action.ToString()} at {c.Time}{NewLine}"
-                    , s => $"{s}```"
-                );
-
         [Command("activity", RunMode = RunMode.Async)]
         public async Task Activity([Summary("The user to get activity info for")] SocketUser user) {
             var events = _eventRetriever.UserEventsById(user.Id);
-            if (events.Any()) await Context.Channel.SendMessageAsync(FormatActivities(events));
+            if (events.Any())
+                await Context.Channel.SendMessageAsync(FormatActivities(events, $"Activities for '{user.Username}'"));
             else await Context.Channel.SendMessageAsync($"Could not find activity for '{user.Username}'.");
         }
     }
