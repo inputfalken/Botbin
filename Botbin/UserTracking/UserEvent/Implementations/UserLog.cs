0820ba674e6545e88eb27c01444f0393ac9dbf63
Robert Andersson <inputfalken@gmail.com>
2017-10-30 22:56:58 +0100

    Update json properties

--- Botbin/UserTracking/UserEvent/Implementations/UserLog.cs
+++ Botbin/UserTracking/UserEvent/Implementations/UserLog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Botbin.UserTracking.UserEvent.Enums;
 using Discord;
+using Newtonsoft.Json;
 
 namespace Botbin.UserTracking.UserEvent.Implementations {
     internal class UserLog : IUserEvent {
@@ -15,24 +16,33 @@ namespace Botbin.UserTracking.UserEvent.Implementations {
 
         public UserLog(IUser user, UserAction type) : this(user, DateTime.Now, type) { }
 
+        [JsonProperty("time")]
         public DateTime Time { get; }
 
+        [JsonProperty("action")]
         public UserAction Action { get; }
 
+        [JsonProperty("id")]
         public ulong Id { get; }
 
+        [JsonProperty("username")]
         public string Username { get; }
 
         public static IEnumerable<IUser> MapToUser(IEnumerable<IUser>users, IEnumerable<IUserEvent> userEvents) =>
             userEvents.Join(users, ue => ue.Id, u => u.Id, (ue, u) => u);
     }
+
     internal sealed class UserMessage : UserLog {
         public UserMessage(IUser user, UserAction type, string message) : base(user, type) => Message = message;
+
+        [JsonProperty("message")]
         public string Message { get; }
     }
 
     internal sealed class UserGame : UserLog {
         public UserGame(IUser user, UserAction type, Game game) : base(user, type) => Game = game;
+
+        [JsonProperty("game")]
         public Game Game { get; }
     }
 }
