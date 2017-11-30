1291f839878845640c0f5e22a5bd24b22d93b188
Robert Andersson <inputfalken@gmail.com>
2017-11-30 22:33:57 +0100

    Add random functionality

--- Botbin/CommandCenters/RandomizerModule.cs
+++ Botbin/CommandCenters/RandomizerModule.cs
@@ -13,8 +13,20 @@ namespace Botbin.CommandCenters {
 
         private const string Oops = "Oops, something went wrong";
 
-        [Command("rnd", RunMode = RunMode.Async)]
+        [Command("random-item", RunMode = RunMode.Async)]
         public async Task RandomizeArgs([Remainder] string message) =>
             await ReplyAsync(_randomizer.Collection(message.Split(null)).ValueOr(Oops));
+
+        [Command("random-number", RunMode = RunMode.Async)]
+        public async Task RandomizeNumber(int max) =>
+            await ReplyAsync(_randomizer.Integer(max).ToString());
+
+        [Command("random-number", RunMode = RunMode.Async)]
+        public async Task RandomizeNumber(int min, int max) =>
+            await ReplyAsync(_randomizer.Integer(min, max).ToString());
+
+        [Command("random-number", RunMode = RunMode.Async)]
+        public async Task RandomizeNumber() =>
+            await ReplyAsync(_randomizer.Integer(0, 101).ToString());
     }
 }
